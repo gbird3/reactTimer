be4ef2544a3110f82912264cde28927d5ca3768c
Gregory Bird <Gregory Bird>
2016-08-17 07:02:21 -0600

    initial commit

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+  render: function () {
+    return (
+      <h2>Countdown Componenet</h2>
+    );
+  }
+});
---
+++ app/components/Timer.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function () {
+    return(
+      <h3>Timer Componenet</h3>
+    );
+  }
+});
