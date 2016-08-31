920b02ff0bf31bc5aec86f7010cc36ec3474f4f1
Gregory Bird <Gregory Bird>
2016-08-31 10:33:46 -0600

    add start timer and countdown test

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -1,8 +1,8 @@
 var expect = require('expect');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var TestUtils = require('react-addons-test-utils');
 var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
 
 var CountdownForm = require('CountdownForm');
 
